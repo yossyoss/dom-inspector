c9c28bb055741d5a625f0b932a3a23fd451560c1
Yshwartz <yshwartz@tremorvideodsp.com>
2018-11-01 09:10:16 +0200

    adding boxes

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
   document.addEventListener("DOMContentLoaded", () =>
     makeDomElelemts(document)
   );
-  const elementToColor = {};
+  const elementToColorMapper = {};
 
   const getRandomColor = () => {
     const letters = "0123456789ABCDEF";
@@ -14,13 +14,13 @@
   };
 
   const getColor = nodeName => {
-    if (elementToColor[nodeName]) {
-      return elementToColor[nodeName];
+    if (elementToColorMapper[nodeName]) {
+      return elementToColorMapper[nodeName];
     }
-    elementToColor[nodeName] = getRandomColor();
-    return elementToColor[nodeName];
+    elementToColorMapper[nodeName] = getRandomColor();
+    return elementToColorMapper[nodeName];
   };
-
+//recursive method the returns an object that represent the dom tree
   const getNodeTree = node => {
     if (node.hasChildNodes()) {
       const children = [];
@@ -41,15 +41,45 @@
 
     return false;
   };
-
-  const createFotter = () => {
-    var div = document.createElement("div");
-    div.style.background = "lightgray";
-    div.style.width = "100%";
-    div.style.height = "400px";
-    document.body.appendChild(div);
+//to-do : make this and createDomInspectorWrapper one generic method
+  const createDomInspectorContainer = () => {
+    const container = document.createElement("container");
+    container.classList.add('di-container');
+    container.style.background = "lightgray";
+    container.style.width = "100%";
+    container.style.padding = "20px";
+    //container.style.height = "300px";
+    container.style.display = "flex";
+    document.body.appendChild(container);
   };
 
+  const createDomInspectorWrapper = () => {
+    const div = document.createElement("div");
+    div.classList.add('di-wrapper');
+    div.innerHTML = 'body';
+    div.style.minWidth = "30px";
+    div.style.minHeight = "30px";
+    div.style.background = '#dc8f8f';
+    div.style.display = "flex";
+    div.style.padding = "10px";
+    const diContainer = document.querySelector('.di-container')
+    diContainer.appendChild(div);
+  }
+
+  const createSingleElementBox = (el) => {
+    console.log(el);
+    
+    const div = document.createElement("div");
+    div.innerHTML = el.nodeName;
+    div.style.minWidth = "30px";
+    div.style.minHeight = "30px";
+    div.style.margin = "10px";
+    div.style.padding = "10px";
+    div.style.background = el.color;
+    const diContainer = document.querySelector('.di-wrapper')
+    diContainer.appendChild(div);
+  }
+  //Here the magic happen
   const makeDomElelemts = document => {
     let bodyEl;
     try {
@@ -58,9 +88,18 @@
       throw new Error("Not a valid html");
     }
     if (bodyEl) {
-      let obj = getNodeTree(bodyEl);
-      console.log(obj);
-      createFotter();
+      let bodyTree = getNodeTree(bodyEl);
+      console.log(bodyTree);
+      createDomInspectorContainer();
+      createDomInspectorWrapper();
+      for (let i = 0; i < bodyTree.children.length; i++) {
+        const element = bodyTree.children[i];
+        createSingleElementBox(element)
+      }
+      // createElementBox(bodyTree.children)
+      // for(let el in bodyTree){
+      //   createElementBox(bodyTree[el]);
+      // } 
       //     body[0].forEach(function(node) {
       //       // Do whatever you want with the node object.
       //       console.log(node);
