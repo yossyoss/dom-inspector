c3e7e8d9eaa10103d16783076d48fb1d5436e6a1
yossyoss <yosi0679@gmail.com>
2018-11-04 01:10:20 +0200

    adding drag events

--- app.js
+++ app.js
@@ -2,6 +2,112 @@
   document.addEventListener("DOMContentLoaded", () =>
     createDomElelemts(document)
   );
+  document.addEventListener(
+    "drag",
+    function(event) {
+      //loopOverTree(event.target);
+      // console.log(event.target.getAttribute("data-di-class"));
+    },
+    false
+  );
+  let dragged;
+  const loopOverTree = el => {
+    // if (el.target.children.length) {
+    //     let clone = el.cloneNode(false);
+    //     el.replaceWith(clone);
+    //el.parentElement.appendChild(clone);
+    //   }
+    // console.log("el", el);
+    // console.log("dragged", dragged);
+
+    if (
+      !el.children.length &&
+      (el.parentNode && el.parentNode.className != "di-body")
+    ) {
+      let clone = el.cloneNode(true);
+      el.replaceWith(clone);
+    } else {
+      //element has children
+      for (let i = 0; i < el.children.length; i++) {
+        // let clone = el.cloneNode(true);
+        // el.replaceWith(clone);
+
+        loopOverTree(el.children[i]);
+      }
+    }
+  };
+  const loopOverTreeEvents = el => {
+    if (
+      !el.children.length &&
+      (el.parentNode && el.parentNode.className != "di-body")
+    ) {
+      el.addEventListener("drag", drag, false);
+      el.addEventListener("dragend", dragend, false);
+      el.addEventListener("dragenter", dragenter, false);
+      el.addEventListener("dragexit", dragexit, false);
+      el.addEventListener("dragleave", dragleave, false);
+      el.addEventListener("dragover", dragover, false);
+      el.addEventListener("dragstart", dragstart, false);
+      el.addEventListener("drop", drop, false);
+    } else {
+      //element has children
+      for (let i = 0; i < el.children.length; i++) {
+        loopOverTree(el.children[i]);
+      }
+    }
+  };
+  const dragstart = event => {
+    //console.log("dragstart");
+    dragged = event.target;
+  };
+  const dragover = event => {
+    //  console.log("dragover");
+    event.preventDefault();
+    event.stopPropagation();
+    //loopOverTree(event.target);
+
+    //console.log("dragover", event.target);
+  };
+  const dragenter = event => {
+    //console.log("dragenter");
+    // console.log(event.target);
+    //event.dataTransfer.dropEffect = "copy";
+    if (dragged == event.target) {
+      return;
+    }
+    event.target.style.border = "2px solid black";
+  };
+  //   div.addEventListener("drop", drop, false);
+  //   div.addEventListener("dragleave", dragleave, false);
+  //   div.addEventListener("dragenter", dragenter, false);
+  //   div.addEventListener("dragover", dragover, false);
+  //   div.addEventListener("dragstart", dragstart, false);
+
+  const dragleave = event => {
+    // console.log("dragleave");
+    event.target.style.border = "1px solid black";
+  };
+  const drag = event => {
+    // console.log("drag");
+  };
+  const dragend = event => {
+    //console.log("dragend");
+    //loopOverTreeEvents(event.target);
+  };
+  const dragexit = event => {
+    //console.log("dragexit");
+  };
+
+  const drop = event => {
+    //console.log("drop");
+    event.preventDefault();
+    event.stopPropagation();
+    if (dragged == event.target) return;
+    // move dragged elem to the selected drop target
+    //   dragged.parentNode.removeChild(dragged);
+    //   event.target.appendChild(dragged);
+    //swapElements(dragged, event.target);
+  };
 
   const elementToColorMapper = {},
     elementClassMapper = {};
@@ -23,14 +129,14 @@
     return elementToColorMapper[nodeName];
   };
 
-  const generateClass = () => {
+  const generateClassName = () => {
     let text = "";
     const possible =
       "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     for (var i = 0; i < 5; i++)
       text += possible.charAt(Math.floor(Math.random() * possible.length));
     if (elementClassMapper[text]) {
-      generateClass();
+      generateClassName();
     } else {
       elementClassMapper[text] = true;
       return text;
@@ -44,8 +150,31 @@
     return document.querySelector(`.${className}`);
   };
 
-  // This is an helper function that I took from here https://css-tricks.com/snippets/javascript/lighten-darken-color/
-  // This function takes colors in hex format (i.e. #F06D06, with or without hash) and lightens or darkens them with a value
+  const swapElements = (el1, el2) => {
+    // save the location of obj2
+    var parent2 = el2.parentNode;
+    var next2 = el2.nextSibling;
+    // special case for el1 is the next sibling of el2
+    if (next2 === el1) {
+      // just put el1 before el2
+      parent2.insertBefore(el1, el2);
+    } else {
+      // insert el2 right before obj1
+      el1.parentNode.insertBefore(el2, el1);
+
+      // now insert el1 where el2 was
+      if (next2) {
+        // if there was an element after el2, then insert el1 right before that
+        parent2.insertBefore(el1, next2);
+      } else {
+        // otherwise, just append as last child
+        parent2.appendChild(el1);
+      }
+    }
+  };
+
+  // This is a function that I took from here https://css-tricks.com/snippets/javascript/lighten-darken-color/
+  // It takes colors in hex format (i.e. #F06D06, with or without hash) and lightens or darkens them with a value
   const getDarkenColor = (colorCode, amount) => {
     let usePound = false;
     if (colorCode[0] == "#") {
@@ -87,17 +216,27 @@
   };
 
   const inspectorMouseOver = e => {
+    e.stopPropagation();
+
     let correspondingDomEl = getCorrespondingDomEl(e);
     highlightParent(correspondingDomEl);
   };
 
   const inspectorMouseOut = e => {
+    e.stopPropagation();
     let correspondingDomEl = getCorrespondingDomEl(e);
     correspondingDomEl.style.background = "none";
+    while (correspondingDomEl) {
+      correspondingDomEl.style.background = "none";
+      correspondingDomEl =
+        correspondingDomEl.parentNode !== document
+          ? correspondingDomEl.parentNode
+          : false;
+    }
   };
 
   const inspectorOnClick = e => {
-    e.preventDefault();
+    e.stopPropagation();
     //console.log(e);
     // These are the default actions (the XPath code might be a bit janky)
     // Really, these could do anything:
@@ -109,7 +248,7 @@
 
   //recursive method the returns an object that represent the dom tree
   const getNodeTree = node => {
-    let unicClassName = generateClass();
+    let unicClassName = generateClassName();
     if (node.hasChildNodes()) {
       const children = [];
       for (let j = 0; j < node.childNodes.length; j++) {
@@ -154,10 +293,23 @@
     div.style.display = "flex";
     div.setAttribute("draggable", "true");
     div.setAttribute("data-di-class", `di-${el.className}`);
-    if (el.nodeName.toLowerCase() === "body") div.classList.add("di-body");
+    if (el.nodeName.toLowerCase() === "body") {
+      div.classList.add("di-body");
+      div.setAttribute("draggable", "false");
+    }
+    div.addEventListener("drag", drag, false);
+    div.addEventListener("dragend", dragend, false);
+    div.addEventListener("dragenter", dragenter, false);
+    div.addEventListener("dragexit", dragexit, false);
+    div.addEventListener("dragleave", dragleave, false);
+    div.addEventListener("dragover", dragover, false);
+    div.addEventListener("dragstart", dragstart, false);
+    div.addEventListener("drop", drop, false);
+
     div.addEventListener("mouseover", inspectorMouseOver, false);
     div.addEventListener("mouseout", inspectorMouseOut, false);
     div.addEventListener("click", inspectorOnClick, false);
+
     return div;
   };
 
